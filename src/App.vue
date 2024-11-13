0fd5714ca812f3470fe37deb2bcfa1394a376d50
likui <1060352728@qq.com>
2024-11-13 21:45:07 +0800

    vue初始化

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+
+<script setup>
+import axios from 'axios';
+import {ref} from "vue";
+
+const count = ref()
+const getData = () => {
+  axios.get('/test/getDate')
+      .then(response => {
+        count.value = response.data
+      });
+}
+</script>
+
+<template>
+  <div>
+    <button @click="getData">点击获取后台参数</button>
+  </div>
+  <div><textarea>{{count}}</textarea></div>
+</template>
+
+<style scoped>
+
+</style>
